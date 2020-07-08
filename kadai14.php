5a365f7c2777a355d6aeabec757f9659703841ad
kishi7829 <66941425+kishi7829@users.noreply.github.com>
2020-07-08 23:04:28 +0900

    Create kadai14.php

---
+++ kadai14.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Profile;
+
+class ProfileController extends Controller
+{
+    //
+    public function add()
+    {
+        return view('admin.profile.create');
+    }
+
+    public function create()
+    {
+        return redirect('admin/profile/create');
+    }
+
+    public function edit()
+    {
+        return view('admin.profile.edit');
+    }
+
+    public function update()
+    {
+        return redirect('admin/profile/edit');
+    }
+    
+    public function create(Request $request)
+    {
+      // 以下を追記
+      // Varidationを行う
+      $this->validate($request , Profile::$rules);
+      
+      $news = new Profile;
+      $form = $request->all();
+      
+      // フォームから送信されてきた_tokenを削除する
+      unset($form['_token']);
+      // フォームから送信されてきたimageを削除する
+      unset($form['image']);
+      
+      // データベースに保存する
+      $news->fill($form);
+      $news->save();
+    }
+}
